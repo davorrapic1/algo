3b28ae7c69a63e2467df74780b8d5b85834648cd
Davor Rapić <rapic.davor@gmail.com>
2022-06-21 10:33:00 +0200

    closestdiff

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // find 2 numbers in arrays  where their sum is closes to target
-const findClosest = (arr, arr2, target, difference) => {
+const findClosest = (arr, arr2, target) => {
   let closest = [];
+  let closestDiff = Infinity;
 
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr2.length; j++) {
       let diff = Math.abs(arr[i] + arr2[j] - target);
-      if (diff <= difference) {
+      if (diff <= closestDiff) {
+        closestDiff = diff;
         closest.push({
           proximity: diff,
           val: `${arr[i]}+${arr2[j]}`,
@@ -19,7 +21,6 @@ const findClosest = (arr, arr2, target, difference) => {
 
 const arr = [1, 7, 4, 11];
 const arr2 = [5, 3, 6, 9];
-const target = 11;
-const biggestDiff = 1;
+const target = 1;
 
-findClosest(arr, arr2, target, biggestDiff);
+findClosest(arr, arr2, target);
